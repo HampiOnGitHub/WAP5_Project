3e5fedba4a717387fe7214eb979b74c022e3cbe4
David Maximilian Vierlinger <s2310238074@fhooe.at>
2025-10-28 11:13:23 +0100

    very simple register form

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,10 +1,56 @@
 import "./Register.css"
+import { useEffect, useRef, useState } from "react"
 
+//mathias braucht den post request auf port 3000
 function Register() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [userName, setUserName] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            userName
+        });
+        
+    }
+
     return (
-        <div>
-            <a>Register site</a>
-        </div>
+        <>
+            <div>
+                <a>Register site</a>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <a>Username</a><br/>
+                <input onChange={(e) => setUserName(e.target.value)}/>
+                <br/>
+                <a>First Name</a><br/>
+                <input onChange={(e) => setFirstName(e.target.value)}/>
+                <br/>
+                <a>Last Name</a><br/>
+                <input onChange={(e) => setLastName(e.target.value)}/>
+                <br/>
+                <a>Email</a><br/>
+                <input onChange={(e) => setEmail(e.target.value)}/>
+                <br/>
+                <a>Password</a><br/>
+                <input type="password" onChange={(e) => setPassword(e.target.value)}/>
+                <br/>
+                <a>Confirm Password</a><br/>
+                <input type="password" onChange={(e) => setConfirmPassword(e.target.value)}/>
+                <br/>
+                <button type="submit" value="submit"> Submit </button>
+
+            </form>
+        </>
     )
 }
 
